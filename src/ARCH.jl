00039d3788aa9f0e6911cea1412e706d6a436fd8
Simon Broda <simon.broda@uzh.ch>
2018-04-03 15:58:23 +0200

    whitespace

--- src/ARCH.jl
+++ src/ARCH.jl
@@ -14,22 +14,22 @@ export ARCHModel, VolatilitySpec, simulate
 const FP = AbstractFloat
 abstract type VolatilitySpec end
 
-struct ARCHModel{VS<:VolatilitySpec,T<:AbstractFloat,df} <: StatisticalModel
+struct ARCHModel{VS<:VolatilitySpec, T<:AbstractFloat, df} <: StatisticalModel
   data::Vector{T}
   coefs::NTuple{df,T}
-  ARCHModel{VS,T,df}(data,coefs) where {VS,T,df}=new(data,coefs)
+  ARCHModel{VS, T, df}(data, coefs) where {VS, T, df}=new(data, coefs)
 end
-ARCHModel{T1<:VolatilitySpec,T2,df}(VS::Type{T1},data::Vector{T2},coefs::NTuple{df,T2})=ARCHModel{VS,T2,df}(data,coefs)
+ARCHModel{T1<:VolatilitySpec, T2, df}(VS::Type{T1}, data::Vector{T2}, coefs::NTuple{df,T2}) = ARCHModel{VS, T2, df}(data, coefs)
 
-loglikelihood{T}(am::ARCHModel{T})=arch_loglik!(T,am.data,zeros(am.data),am.coefs...)
-nobs(am::ARCHModel)=length(am.data)
-fit{T}(AM::Type{ARCHModel{T}},data)=fit(T,data)
+loglikelihood{T}(am::ARCHModel{T}) = arch_loglik!(T, am.data, zeros(am.data), am.coefs...)
+nobs(am::ARCHModel) = length(am.data)
+fit{T}(AM::Type{ARCHModel{T}}, data) = fit(T,data)
 
-function simulate{T1<:VolatilitySpec,T2<:AbstractFloat,N}(VS::Type{T1},nobs,coefs::NTuple{N,T2})
-  const warmup=100
-  data=zeros(T2,nobs+warmup)
-  ht=zeros(T2,nobs+warmup)
-  archsim!(VS,data,ht,coefs...)
+function simulate{T1<:VolatilitySpec, T2<:AbstractFloat, N}(VS::Type{T1}, nobs, coefs::NTuple{N,T2})
+  const warmup = 100
+  data = zeros(T2, nobs+warmup)
+  ht = zeros(T2, nobs+warmup)
+  archsim!(VS, data, ht, coefs...)
   data[warmup+1:warmup+nobs]
 end
 
