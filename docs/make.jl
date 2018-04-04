176b857fc55e030bd015f227592fa6cbde3fb504
Simon Broda <simon.broda@uzh.ch>
2018-04-04 17:04:32 +0200

    fix Documenter

--- docs/make.jl
+++ docs/make.jl
@@ -3,6 +3,6 @@ makedocs(modules=[ARCH],
         doctest=true)
 
 deploydocs(deps   = Deps.pip("mkdocs", "python-markdown-math"),
-    repo = "https://github.com/s-broda/ARCH.jl.git",
+    repo = "github.com/s-broda/ARCH.jl.git",
     julia  = "0.6",
     osname = "linux")
