498afeef2651a9690e460f94b4a6572f6a366d79
Simon Broda <simon.broda@uzh.ch>
2018-04-26 16:40:29 +0200

    use CartesianRange instead of Base.Cartesian

--- src/ARCH.jl
+++ src/ARCH.jl
@@ -106,26 +106,17 @@ fit!(AM::ARCHModel{VS}, algorithm=BFGS; kwargs...) where {VS<:VolatilitySpec} = 
 fit(::Type{VS}, data, algorithm=BFGS; kwargs...) where VS<:VolatilitySpec = (ht = zeros(data); coefs=startingvals(VS, data); fit!(ht, coefs, VS, data, algorithm; kwargs...); return ARCHModel(VS, data, ht, coefs))
 fit(AM::ARCHModel{VS}, algorithm=BFGS; kwargs...) where {VS<:VolatilitySpec} = (AM2=ARCHModel(VS, AM.data, AM.coefs); fit!(AM2, algorithm=BFGS; kwargs...); return AM2)
 
-
-
 function selectmodel(::Type{VS}, data::Vector{T}, maxpq=3, args...; criterion=bic, kwargs...) where {VS<:VolatilitySpec, T<:AbstractFloat}
-    #ndims = length(Base.unwrap_unionall(VS).parameters) #e.g., two (p and q) for GARCH{p, q}
     ndims =my_unwrap_unionall(VS)#e.g., two (p and q) for GARCH{p, q}
-    res = _selectmodel(VS, Val{ndims}(), Val{maxpq}(), data)
+    res =Array{ARCHModel, ndims}(ntuple(i->maxpq, ndims))
+    for ind in CartesianRange(size(res))
+        res[ind] = fit(VS{ind.I...}, data)
+    end
     crits = criterion.(res)
     _, ind = findmin(crits)
     return res[ind]
 end
 
-@generated function _selectmodel(VS, ::Val{ndims}, ::Val{maxpq}, data) where {ndims, maxpq}
-    quote
-        res =Array{ARCHModel, $ndims}(@ntuple($ndims, i->$maxpq))
-        @nloops $ndims i res begin
-        @nref($ndims, res, i) = fit(VS{@ntuple($ndims, i)...}, data)
-    end
-    return res
-end
-end
 
 #count the number of type vars. there's probably a better way.
 function my_unwrap_unionall(a::ANY)
