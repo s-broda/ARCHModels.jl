37868494004f9d4ec29429134dcb0987e5d40ab2
Simon Broda <simon.broda@uzh.ch>
2018-05-07 13:36:30 +0200

    fix test for unicode output for real

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,7 +18,7 @@ am3=fit(am2)
 @test dof(am) == 3
 @test coefnames(GARCH{1, 1}) == ["ω", "β₁", "α₁"]
 @test all(isapprox.(coef(am), [0.9086850084210619, 0.9055267307122488, 0.050365843108442374], rtol=1e-4))
-@test all(isapprox.(stderror(am), [0.14583357347889914, 0.01035533071207874, 0.005222909457230848], rtol=1e-4))
+@test all(isapprox.(stderr(am), [0.14583357347889914, 0.01035533071207874, 0.005222909457230848], rtol=1e-4))
 @test all(am2.coefs .== am.coefs)
 @test all(am3.coefs .== am2.coefs)
 e = @test_throws ARCH.NumParamError ARCH.loglik!(ht, spec, data, [0., 0., 0., 0.])
