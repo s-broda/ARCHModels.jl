db3d06f9167dfc7422fa346e10e70c55ac466346
Simon Broda <simon.broda@uzh.ch>
2018-05-17 19:39:20 +0200

    add distribution tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,44 +6,47 @@ spec = GARCH{1, 1};
 coefs = [1., .9, .05];
 srand(1);
 data = simulate(spec, T, coefs);
+srand(1);
+datat = simulate(spec, T, coefs, StdTDist(4))
 ht = zeros(data);
 am = selectmodel(GARCH, data)
 am2 = ARCHModel(spec, data, ht, coefs)
 fit!(am2)
 am3 = fit(am2)
-dist=StdNormal{}
-d = StdNormal()
-
-@test loglikelihood(ARCHModel(spec, data, coefs)) ==  ARCH.loglik!(ht, spec, dist, data, coefs)
+am4 = selectmodel(GARCH, datat, StdTDist)
+am5 = fit(GARCH{3, 0}, data)
+@test loglikelihood(ARCHModel(spec, data, coefs)) ==  ARCH.loglik!(ht, spec, StdNormal{}, data, coefs)
 @test nobs(am) == T
 @test dof(am) == 3
+
 @test coefnames(GARCH{1, 1}) == ["ω", "β₁", "α₁"]
 @test all(isapprox.(coef(am), [0.9086850084210619, 0.9055267307122488, 0.050365843108442374], rtol=1e-4))
 @test all(isapprox.(stderr(am), [0.14583357347889914, 0.01035533071207874, 0.005222909457230848], rtol=1e-4))
 @test all(am2.coefs .== am.coefs)
 @test all(am3.coefs .== am2.coefs)
+@test all(isapprox(coef(am4), [0.8306902920885605, 0.9189514425541352, 0.04207946140844637, 3.8356660627658075], rtol=1e-4))
 
-e = @test_throws ARCH.NumParamError ARCH.loglik!(ht, spec, dist, data, [0., 0., 0., 0.])
+@test_warn "Fisher" stderr(am5);
+e = @test_throws ARCH.NumParamError ARCH.loglik!(ht, spec, StdNormal{}, data, [0., 0., 0., 0.])
 str = sprint(showerror, e.value)
 @test startswith(str, "incorrect number of parameters")
-@test_throws ARCH.NumParamError ARCH.sim!(ht, spec, d, data, [0., 0., 0., 0.])
+@test_throws ARCH.NumParamError ARCH.sim!(ht, spec, StdNormal(), data, [0., 0., 0., 0.])
 e = @test_throws ARCH.LengthMismatchError ARCHModel(spec, data, coefs, coefs)
 str = sprint(showerror, e.value)
 @test startswith(str, "length of arrays does not match")
-@test selectmodel(ARCH._ARCH, data).coefs == fit(ARCH._ARCH{3}, data).coefs
 io = IOBuffer()
 str = sprint(io -> show(io, am))
 @test startswith(str, "\nGARCH{1,1}")
 
+@test selectmodel(ARCH._ARCH, data).coefs == fit(ARCH._ARCH{3}, data).coefs
 
-@test ARCH.constraints(StdNormal, Float64) == (Float64[], Float64[])
-srand(1)
-data = rand(d, 10000)
-@test ARCH.logkernel(StdNormal, data[1], []) ≈ -0.044190072874578434
-@test ARCH.nparams(StdNormal) == 0
-@test ARCH.logconst(StdNormal, []) ≈ -0.9189385332046728
 
-d = StdTDist(4)
+@test fit(StdNormal, data).coefs == Float64[]
+@test coefnames(StdNormal) == String[]
+@test ARCH.distname(StdNormal) == "Gaussian"
+
 srand(1)
-data = rand(d, 10000)
+data = rand(StdTDist(4), 10000)
 @test fit(StdTDist, data).coefs[1] ≈ 3.972437329588246 rtol=1e-4
+@test coefnames(StdTDist) == ["ν"]
+@test ARCH.distname(StdTDist) == "Student's t"
