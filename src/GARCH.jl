ce807f5c8b352e5b5fd2d2ffb55c94c2da44371a
Simon Broda <simon.broda@uzh.ch>
2018-05-18 19:46:02 +0200

    pass around instances of GARCH

--- src/GARCH.jl
+++ src/GARCH.jl
@@ -1,13 +1,19 @@
 export GARCH
 export _ARCH #ARCH conflicts with module name
-struct GARCH{p, q} <: VolatilitySpec end
+struct GARCH{p, q, T<:AbstractFloat} <: VolatilitySpec{T}
+    coefs::Vector{T}
+    GARCH{p, q, T}(coefs::Vector{T}) where {p, q, T}  = (length(coefs) == nparams(GARCH{p, q})  || throw(NumParamError(nparams(GARCH{p, q}), length(coefs))); new{p, q, T}(coefs))
+end
+GARCH{p, q}(coefs::Vector{T}) where {p, q, T}  = GARCH{p, q, T}(coefs)
+
+
 const _ARCH = GARCH{0}
 
-@inline nparams(::Type{GARCH{p,q}}) where {p, q} = p+q+1
+@inline nparams(::Type{<:GARCH{p, q}}) where {p, q} = p+q+1
 
-@inline presample(::Type{GARCH{p,q}}) where {p, q} = max(p, q)
+@inline presample(::Type{<:GARCH{p, q}}) where {p, q} = max(p, q)
 
-@inline function update!(ht, ::Type{GARCH{p,q}}, data, coefs, t) where {p, q}
+@inline function update!(ht, ::Type{<:GARCH{p,q}}, data, coefs, t) where {p, q}
     ht[t] = coefs[1]
     for i = 1:p
         ht[t] += coefs[i+1]*ht[t-i]
@@ -18,7 +24,7 @@ const _ARCH = GARCH{0}
     return nothing
 end
 
-@inline function uncond(::Type{GARCH{p, q}}, coefs::Vector{T}) where {p, q, T}
+@inline function uncond(::Type{<:GARCH{p, q}}, coefs::Vector{T}) where {p, q, T}
     den=one(T)
     for i = 1:p+q
         den -= coefs[i+1]
@@ -26,7 +32,7 @@ end
     h0 = coefs[1]/den
 end
 
-function startingvals(::Type{GARCH{p,q}}, data::Array{T}) where {p, q, T}
+function startingvals(::Type{<:GARCH{p,q}}, data::Array{T}) where {p, q, T}
     x0 = zeros(T, p+q+1)
     x0[2:p+1] = 0.9/p
     x0[p+2:end] = 0.05/q
@@ -34,14 +40,14 @@ function startingvals(::Type{GARCH{p,q}}, data::Array{T}) where {p, q, T}
     return x0
 end
 
-function constraints(::Type{GARCH{p,q}}, ::Type{T}) where {p, q, T}
+function constraints(::Type{<:GARCH{p,q}}, ::Type{T}) where {p, q, T}
     lower = zeros(T, p+q+1)
     upper = ones(T, p+q+1)
     upper[1] = T(Inf)
     return lower, upper
 end
 
-function coefnames(::Type{GARCH{p,q}}) where {p, q}
+function coefnames(::Type{<:GARCH{p,q}}) where {p, q}
     names = Array{String, 1}(p+q+1)
     names[1] = "ω"
     names[2:p+1] .= (i->"β"*subscript(i)).([1:p...])
