78c3e83f6fbea061276fb709f4ca39ae6b42604c
Simon Broda <simon.broda@uzh.ch>
2018-06-18 18:45:43 +0200

    update comment

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,8 @@ using Base.Test
 using ARCH
 #=
 Data from [1]. See [2] for a comparsion of GARCH software based on this data.
-[1] Bollerslev, T. and Ghysels, E. (1996), Periodic Autoregressive Conditional Heteroscedasticity, Journal of Business and Economic Statistics (14), pp. 139-151.
-[2] Brooks, C., Burke, S. P., and Persand, G. (2001), Benchmarks and the accuracy of GARCH model estimation, International Journal of Forecasting (17), pp. 45-56.
+[1] Bollerslev, T. and Ghysels, E. (1996), Periodic Autoregressive Conditional Heteroscedasticity, Journal of Business and Economic Statistics (14), pp. 139-151. https://doi.org/10.2307/1392425 
+[2] Brooks, C., Burke, S. P., and Persand, G. (2001), Benchmarks and the accuracy of GARCH model estimation, International Journal of Forecasting (17), pp. 45-56. https://doi.org/10.1016/S0169-2070(00)00070-4
 =#
 #using HTTP
 #res=HTTP.get("http://people.stern.nyu.edu/wgreene/Text/Edition7/TableF20-1.txt")
