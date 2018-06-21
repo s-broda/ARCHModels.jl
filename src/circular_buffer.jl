049b0680d67dbf10981e614a64e61d8643f61359
Simon Broda <simon.broda@uzh.ch>
2018-06-21 15:35:04 +0200

    import DataStructures

--- src/circular_buffer.jl
+++ src/circular_buffer.jl
@@ -1,20 +1,3 @@
-"""
-New items are pushed to the back of the list, overwriting values in a circular fashion.
-"""
-mutable struct CircularBuffer{T} <: AbstractVector{T}
-    capacity::Int
-    first::Int
-    length::Int
-    buffer::Vector{T}
-
-    CircularBuffer{T}(capacity::Int) where {T} = new{T}(capacity, 1, 0, Vector{T}(undef, capacity))
-end
-
-function Base.empty!(cb::CircularBuffer)
-    cb.length = 0
-    cb
-end
-
 Base.@propagate_inbounds function _buffer_index_checked(cb::CircularBuffer, i::Int)
     @boundscheck if i < 1 || i > cb.length
         throw(BoundsError(cb, i))
@@ -41,15 +24,6 @@ end
     cb
 end
 
-function Base.pop!(cb::CircularBuffer)
-    if cb.length == 0
-        throw(ArgumentError("array must be non-empty"))
-    end
-    i = _buffer_index(cb, cb.length)
-    cb.length -= 1
-    cb.buffer[i]
-end
-
 @inline function Base.push!(cb::CircularBuffer, data)
     # if full, increment and overwrite, otherwise push
     if cb.length == cb.capacity
@@ -60,40 +34,3 @@ end
     cb.buffer[_buffer_index(cb, cb.length)] = data
     cb
 end
-
-function Compat.popfirst!(cb::CircularBuffer)
-    if cb.length == 0
-        throw(ArgumentError("array must be non-empty"))
-    end
-    i = cb.first
-    cb.first = (cb.first + 1 > cb.capacity ? 1 : cb.first + 1)
-    cb.length -= 1
-    cb.buffer[i]
-end
-
-function Compat.pushfirst!(cb::CircularBuffer, data)
-    # if full, decrement and overwrite, otherwise pushfirst
-    if cb.length < cb.capacity
-        cb.length += 1
-    end
-    cb.first = (cb.first == 1 ? cb.length : cb.first - 1)
-    cb.buffer[cb.first] = data
-    cb
-end
-
-function Base.append!(cb::CircularBuffer, datavec::AbstractVector)
-    # push at most last `capacity` items
-    n = length(datavec)
-    for i in max(1, n-capacity(cb)+1):n
-        push!(cb, datavec[i])
-    end
-    cb
-end
-
-Base.length(cb::CircularBuffer) = cb.length
-Base.size(cb::CircularBuffer) = (length(cb),)
-Base.convert(::Type{Array}, cb::CircularBuffer{T}) where {T} = T[x for x in cb]
-Base.isempty(cb::CircularBuffer) = cb.length == 0
-
-capacity(cb::CircularBuffer) = cb.capacity
-isfull(cb::CircularBuffer) = length(cb) == cb.capacity
