e043f3876e3a8b4b19bd9cc442adab06f8bbc22d
Simon Broda <simon.broda@uzh.ch>
2018-10-26 17:36:41 +0200

    turn off threading for now to avoid intermittent segfaults

--- src/ARCH.jl
+++ src/ARCH.jl
@@ -534,19 +534,20 @@ function selectmodel(::Type{VS}, data::Vector{T};
                      ) where {VS<:VolatilitySpec, T<:AbstractFloat,
                               SD<:StandardizedDistribution, MS<:MeanSpec
                               }
-    mylock=Threads.SpinLock()
+    #mylock=Threads.SpinLock()
     ndims = my_unwrap_unionall(VS)-1#e.g., two (p and q) for GARCH{p, q, T}
     res = Array{ARCHModel, ndims}(undef, ntuple(i->maxlags, ndims))
-    Threads.@threads for ind in collect(CartesianIndices(size(res)))
+    #Threads.@threads
+	for ind in collect(CartesianIndices(size(res)))
         res[ind] = fit(VS{ind.I...}, data; dist=dist, meanspec=meanspec,
                        algorithm=algorithm, autodiff=autodiff, kwargs...)
         if show_trace
-            lock(mylock)
+            #lock(mylock)
             Core.println(modname(VS{ind.I...}), " model has ",
                               uppercase(split("$criterion", ".")[2]), " ",
                               criterion(res[ind]), "."
                               )
-            unlock(mylock)
+            #unlock(mylock)
         end
     end
     crits = criterion.(res)
