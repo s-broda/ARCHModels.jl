5db1690b293076203f014b6c8109894b001a3b7b
Simon Broda <simon.broda@uzh.ch>
2019-03-04 17:30:00 +0100

    restructure file layout

---
+++ src/utils.jl
@@ -0,0 +1,28 @@
+struct NumParamError <: Exception
+    expected::Int
+    got::Int
+end
+
+function showerror(io::IO, e::NumParamError)
+    print(io, "incorrect number of parameters: expected $(e.expected), got $(e.got).")
+end
+
+Base.@irrational sqrt2invpi 0.79788456080286535587 sqrt(big(2)/big(π))
+
+#from here https://stackoverflow.com/questions/46671965/printing-variable-subscripts-in-julia
+subscript(i::Integer) = i<0 ? error("$i is negative") : join('₀'+d for d in reverse(digits(i)))
+
+function modname(::Type{VS}) where VS<:VolatilitySpec
+    s = "$(VS)"
+    s = s[1:findlast(isequal(','), s)-1] * '}'
+end
+
+#count the number of type vars. there's probably a better way.
+function my_unwrap_unionall(@nospecialize a)
+    count = 0
+    while isa(a, UnionAll)
+        a = a.body
+        count += 1
+    end
+    return count
+end
