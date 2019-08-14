450d75a1de9796139790d846c1ec35e2d7daab3c
Simon Broda <simon.broda@uzh.ch>
2019-08-14 16:10:33 +0200

    fix scope issue

--- deps/build.jl
+++ deps/build.jl
@@ -11,6 +11,7 @@ tickers = ["AAPL", "IBM", "XOM", "KO", "MSFT", "INTC", "MRK", "PG", "VZ", "WBA",
 alldata = zeros(2786, 29)
 for (j, ticker) in enumerate(tickers)
         @repeat 4 try
+            @info "...$ticker"
             filename = joinpath(datadir, "$ticker.csv")
             isfile(joinpath(datadir, "$ticker.csv")) || download("http://quotes.wsj.com/$ticker/historical-prices/download?num_rows=100000000&range_days=100000000&startDate=03/19/2008&endDate=04/11/2019", filename)
             data = parse.(Float64, readdlm(joinpath(datadir, "$ticker.csv"), ',', String, skipstart=1)[:, 5])
@@ -20,8 +21,8 @@ for (j, ticker) in enumerate(tickers)
         catch e
             @delay_retry if 1==1 end
         end
-        alldata = 100 * diff(log.(alldata), dims=1)
-        open(joinpath(datadir, "dow29.csv"), "w") do io
-             writedlm(io, alldata, ',')
-        end
+end
+alldata = 100 * diff(log.(alldata), dims=1)
+open(joinpath(datadir, "dow29.csv"), "w") do io
+     writedlm(io, alldata, ',')
 end
