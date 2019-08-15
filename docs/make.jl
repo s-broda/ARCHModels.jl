ba0fd4204371b46a0617e77a24876c563865d15e
Simon Broda <simon.broda@uzh.ch>
2019-08-15 19:13:08 +0200

    compatibility with Documenter 0.23.0

--- docs/make.jl
+++ docs/make.jl
@@ -1,5 +1,6 @@
 push!(LOAD_PATH,"../src/")
 using Documenter, ARCHModels
+DocMeta.setdocmeta!(ARCHModels, :DocTestSetup, :(using ARCHModels; using Random; Random.seed!(1)); recursive=true)
 makedocs(modules=[ARCHModels],
         sitename="ARCHModels.jl",
         format = Documenter.HTML(assets=["assets/invenia.css"]),
--- docs/src/reference.md
+++ docs/src/reference.md
@@ -6,20 +6,10 @@
 
 ## Public API
 ```@meta
-DocTestSetup = quote
-    using ARCHModels
-    using Random
-    Random.seed!(1)
-end
 DocTestFilters = r".*[0-9\.]"
 ```
 
 ```@autodocs
 Modules = [ARCHModels]
 Private = false
-```
-
-```@meta
-DocTestSetup = nothing
-DocTestFilters = nothing
 ```
